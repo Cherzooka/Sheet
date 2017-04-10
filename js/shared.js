ad7b7a2d64a1066c63fca04fe301650eaabcbc80
Cherzooka <lady.cherette@gmail.com>
2017-04-09 23:23:00 -0500

    //

--- js/shared.js
+++ js/shared.js
@@ -1,85 +1,93 @@
-      (function($) {
-        $(document).ready(function() {
-          $("[title]").style_my_tooltips({
-            tip_follows_cursor: true,
-            tip_delay_time: 90,
-            tip_fade_speed: 600,
-            attribute: "title"
-          });
-        });
-      })(jQuery);
-    
-      $(document).ready(function(){
-        $(".equip").click(function(){
-          $(".equipment").toggle("slow");
-        });
-      });
-      
-          $(document).ready(function() {
-      // init Isotope
-      var $grid = $('div#inventory').imagesLoaded( function() {
-      $grid.isotope({
-        itemSelector: '.item',
-        layoutMode: 'masonry',
-        percentPosition: true,
-        masonry: {
-          columnWidth: '.item'
-        },
-        getSortData: {
-          name: '.item-name',
-          count: '.item-number',
-          weight: '.item-weight',
-          value: '.item-value',
-          type: '.item-type'
-        }
-      });
-      });
-      
-          // sort items on button click
-    $('.sort-by-button-group').on( 'click', 'button', function() {
-      var sortByValue = $(this).attr('data-sort-by');
-      $grid.isotope({ sortBy: sortByValue });
-    });
-      
-      // store filter for each group
-      var filters = {};
+     $(document).ready(function() {
+       $(".equip").click(function() {
+         $(".equipment").toggle("slow");
+       });
 
-      $('.filters').on('click', '.button', function() {
-        var $this = $(this);
-        // get group key
-        var $buttonGroup = $this.parents('.button-group');
-        var filterGroup = $buttonGroup.attr('data-filter-group');
-        // set filter for group
-        filters[filterGroup] = $this.attr('data-filter');
-        // combine filters
-        var filterValue = concatValues(filters);
-        // set filter for Isotope
-        $grid.isotope({
-          filter: filterValue
-        });
-      });
+       $("[title]").style_my_tooltips({
+         tip_follows_cursor: true,
+         tip_delay_time: 90,
+         tip_fade_speed: 600,
+         attribute: "title"
+       });
 
-      // change is-checked class on buttons
-      $('.button-group').each(function(i, buttonGroup) {
-        var $buttonGroup = $(buttonGroup);
-        $buttonGroup.on('click', 'button', function() {
-          $buttonGroup.find('.is-checked').removeClass('is-checked');
-          $(this).addClass('is-checked');
-        });
-      });
-    });
-    
-    // layout Isotope after each image loads
-    var $grid = $('div#inventory');
-      $grid.imagesLoaded().progress( function() {
-      $grid.isotope('layout');
-    });
+       // init Isotope
+       var $grid = $('div#inventory').imagesLoaded(function() {
+         $grid.isotope({
+           itemSelector: '.item',
+           layoutMode: 'masonry',
+           percentPosition: true,
+           masonry: {
+             columnWidth: '.item'
+           },
+           getSortData: {
+             name: '.item-name',
+             count: '.item-number parseInt',
+             weight: '.item-weight parseInt',
+             value: '.item-value parseInt',
+             type: '.item-type'
+           },
+           sortAscending: {
+            name: true,
+            count: false,
+            weight: false,
+            value: false,
+          },
+           sortBy: ['name', 'weight']
+         });
+       });
+       
+         $('#sorts').on( 'click', 'button', function() {
+           /* Get the element name to sort */
+           var sortValue = $(this).attr('data-sort-by');
+           /* Get the sorting direction: asc||desc */
+           var sortDirection = $(this).attr('data-sort-direction');
+           /* convert it to a boolean */
+           sortDirection = sortDirection == 'asc';
+           /* pass it to isotope */
+           $grid.isotope({ sortBy: sortValue, sortAscending: sortDirection });
+         });
 
-    // flatten object by concatting values
-    function concatValues(obj) {
-      var value = '';
-      for (var prop in obj) {
-        value += obj[prop];
-      }
-      return value;
-    }
+       // bind sort button click
+       $('#sorts').on('click', 'button', function() {
+         var sortByValue = $(this).attr('data-sort-by');
+         $grid.isotope({
+           sortBy: sortByValue
+         });
+       });
+
+       // store filter for each group
+       var filters = {};
+
+       $('#filters').on('click', '.button', function() {
+         var $this = $(this);
+         // get group key
+         var $buttonGroup = $this.parents('.button-group');
+         var filterGroup = $buttonGroup.attr('data-filter-group');
+         // set filter for group
+         filters[filterGroup] = $this.attr('data-filter');
+         // combine filters
+         var filterValue = concatValues(filters);
+         // set filter for Isotope
+         $grid.isotope({
+           filter: filterValue
+         });
+       });
+
+       // change is-checked class on buttons
+       $('.button-group').each(function(i, buttonGroup) {
+         var $buttonGroup = $(buttonGroup);
+         $buttonGroup.on('click', 'button', function() {
+           $buttonGroup.find('.is-checked').removeClass('is-checked');
+           $(this).addClass('is-checked');
+         });
+       });
+
+       // flatten object by concatting values
+       function concatValues(obj) {
+         var value = '';
+         for (var prop in obj) {
+           value += obj[prop];
+         }
+         return value;
+       }
+     });
